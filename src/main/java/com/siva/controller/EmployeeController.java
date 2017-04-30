af0409aae2150f74ffa448db6449c947b3bbad89
lenovo <lenovo@DESKTOP-O11NCFK>
2017-04-30 16:46:01 +0530

    initial Get setup

---
+++ src/main/java/com/siva/controller/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.siva.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class EmployeeController {
+	private static Logger _LOGGER = Logger.getLogger(EmployeeController.class);
+	
+	@RequestMapping(name="/login")
+	public ModelAndView login(){
+		_LOGGER.info("Enter the Login Controller");
+		return null;
+	}
+	
+}
---
+++ src/main/java/com/siva/model/RegisterModel.java
@@ -0,0 +1,41 @@
+package com.siva.model;
+
+public class RegisterModel {
+	private String userName;
+	private String password;
+	private String mailId;
+	private String mobileNum;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMailId() {
+		return mailId;
+	}
+
+	public void setMailId(String mailId) {
+		this.mailId = mailId;
+	}
+
+	public String getMobileNum() {
+		return mobileNum;
+	}
+
+	public void setMobileNum(String mobileNum) {
+		this.mobileNum = mobileNum;
+	}
+
+}
